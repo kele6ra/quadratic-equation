fcb47d5e6247716136e1205a84535fbe2ce11e17
unknown <Andrei Auchinnikau>
2018-02-21 23:09:20 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var a = +(equation.substring(0, equation.indexOf('*')-1)).replace(/\s+/g, '');
+  var b = +(equation.substring(equation.indexOf('^2')+2, equation.indexOf('*', equation.indexOf('^2'))-1)).replace(/\s+/g, '');
+  var c = +(equation.substring(equation.indexOf('x', equation.indexOf('^2'))+1)).replace(/\s+/g, '');
+
+  var D = Math.pow(b,2) - 4*a*c;
+  var res =[];
+  
+  if (D>0){
+    res.push(Math.round(((-b)+Math.sqrt(D))/(2*a)));
+    res.push(Math.round(((-b)-Math.sqrt(D))/(2*a)));
+	res.sort(function(a,b) {return a - b; });
+  }
+
+  return (res);
 }
